dca0fd516a8349e7c37382f852e5ff3cba869546
Manicardi Daniele <Manicardi Daniele@DESKTOP-N93NFUG>
2016-08-28 00:26:11 +0200

    Riadattamento delle pagine del SuperUser sul ceo

--- pages/ceo/inserisci_tutor/ajaxOpsPerTutor/ajaxInvia.php
+++ pages/ceo/inserisci_tutor/ajaxOpsPerTutor/ajaxInvia.php
@@ -9,15 +9,16 @@
     $cognome = $connection->escape_string($_POST['cognome']);
     $telefono = $connection->escape_string($_POST['telefono']);
     $email = $connection->escape_string($_POST['email']);
-    $idAzienda = $_SESSION['userId'];
+    
+    $Query = "INSERT INTO `tutor` (`username`, `password`, `nome`, `cognome`, `telefono`, `email`, `azienda_id_azienda`) "
+            . "VALUES ('$username', '$password', '$nome', '$cognome', '$telefono', '$email', ".$_SESSION['userId'].")";
     
     
-    $Query = "INSERT INTO `tutor` (`username`, `password`, `nome`, `cognome`, `telefono`, `email`,`azienda_id_azienda`) "
-            . "VALUES ('$username', '$password', '$nome', '$cognome', '$telefono', '$email','$idAzienda');";
     
-    if(!$connection->query($Query))
-        echo "Inserimento dei dati NON riuscito";
+    if (!$connection->query($Query))
+        echo "Inserimento dati NON riuscito";
     else
         echo "Inserimento dei dati riuscito!";
+    
     
 
