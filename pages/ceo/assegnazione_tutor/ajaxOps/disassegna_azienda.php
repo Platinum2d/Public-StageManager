598ef92f24bfcabcb3016e75d0d7414bb76623c9
Manicardi Daniele <Manicardi Daniele@DESKTOP-N93NFUG>
2016-08-28 00:30:49 +0200

    eliminazione della vecchia sezione di assegnazione e inserimento

--- pages/ceo/assegnazione_tutor/ajaxOps/disassegna_azienda.php
+++
@@ -1,6 +0,0 @@
-<?php
-    include '../../../functions.php';   
-    $uid = intval ( $_POST ['uid'] );
-    $db = dbConnection("../../../../");
-    $q_studenti = $db->query ( "Update studente set tutor_id_tutor = NULL WHERE id_studente = $uid" );
-?>
