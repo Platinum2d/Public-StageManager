1dc687190116ec4a571de8bed40800c0488e5c2d
broskh <alessio.scheri@gmail.com>
2016-11-14 22:39:55 +0100

    Migliorata leggibilità query in un file del profilo utente dell'utente studente.

--- pages/studente/profiloutente/ajaxOps/ajaxCheckPassword.php
+++ pages/studente/profiloutente/ajaxOps/ajaxCheckPassword.php
@@ -6,7 +6,11 @@
     $idstudente = $_SESSION['userId'];
     $password = md5($_POST['password']);
     
-    $query = "SELECT `id_studente` FROM `studente`, `utente` WHERE `id_studente` = $idstudente AND `id_utente` = $idstudente AND `password` = '$password'";
+    $query = "SELECT studente.id_studente 
+                FROM studente, utente 
+                WHERE studente.id_studente = $idstudente  
+                AND utente.id_utente = studente.id_studente
+                AND password = '$password'";
     $result = $conn->query($query);
     if (!$result || $result->num_rows === 0)
         echo "non esiste";
