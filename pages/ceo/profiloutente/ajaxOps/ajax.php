2dccac8b965cd6f6c7356ea8b9e33fcf33e23bc6
Platinum2d <manicardi215@gmail.com>
2017-02-26 12:15:57 +0100

    Corretto bug nel profilo utente. Il file ajax non aggiornava i dati perchè i parametri di goBack e l'inclusione di function erano errati.

--- pages/ceo/profiloutente/ajaxOps/ajax.php
+++ pages/ceo/profiloutente/ajaxOps/ajax.php
@@ -1,13 +1,16 @@
 <?php
-    include '../../functions.php';
+    include '../../../functions.php';
     if ($_POST ['first']) {
-        $connessione = dbConnection ("../../../");
+        $connessione = dbConnection ("../../../../");
         $id_az = $_SESSION ['userId'];
         $nome = $connessione->escape_string ( strip_tags($_POST ['first']));
         $cognome = $connessione->escape_string ( strip_tags($_POST ['last']) );
         $email = $connessione->escape_string ( strip_tags($_POST ['mail']) );
         $telefono = $connessione->escape_string ( strip_tags($_POST ['phone']) );
-        $sql = "update azienda set nome_responsabile='$nome',cognome_responsabile='$cognome', email_responsabile='$email', telefono_responsabile='$telefono' where id_azienda='$id_az';";
-        $result = $connessione->query ( $sql );
+        $sql = "UPDATE azienda SET nome_responsabile='$nome',cognome_responsabile='$cognome', email_responsabile='$email', telefono_responsabile='$telefono' WHERE id_azienda='$id_az';";
+        if ($connessione->query ( $sql ))
+            echo "ok";
+        else 
+            echo $connessione->error;
     }
 ?>
