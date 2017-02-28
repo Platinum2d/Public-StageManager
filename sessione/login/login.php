36fc8108883f4d2d323c33fe43f5ee6ef260bf03
broskh <alessio.scheri@gmail.com>
2017-02-28 16:49:05 +0100

    Snellite query in fase di login.

--- sessione/login/login.php
+++ sessione/login/login.php
@@ -1,96 +1,41 @@
 <?php
     include '../../pages/functions.php';
-    function sessionInformation($usr, $userId, $type) {
-        $_SESSION ['user'] = $usr;
-        $_SESSION ['userId'] = $userId;
-        $_SESSION ['type'] = $type;
-    }
     
-    $usr = $_POST ["user"];
-    $psw = MD5 ( $_POST ["password"] );
     $connessione = dbConnection ("../../");
-    $usr = $connessione->escape_string($usr);
+    $usr = $connessione->escape_string($_POST ["user"]);
+    $psw = MD5 ( $_POST ["password"] );
     $tipo = -1;
     
-    $query = "SELECT id_utente, username, password
+    $query = "SELECT id_utente, username, password, tipo_utente
                 FROM utente
-                WHERE username='$usr' AND password='$psw' "
-            . " AND tipo_utente = 0;";
+                WHERE username='$usr' AND password='$psw';";
     $result = $connessione->query ( $query );
     
-    $numrows = ($result && $result->num_rows > 0) ? $result->num_rows : 0;
-    
-    if ($numrows > 0)
+    if ($result && $result->num_rows > 0)
     {
-        $tipo = 0;
         $row = $result->fetch_assoc();
-        $userId = $row ['id_utente'];
-        $table = "super_user";
-    }
+        $_SESSION ['user'] = $usr;
+        $_SESSION ['userId'] = intval ($row ['id_utente']);
+        $_SESSION ['type'] = intval ($row ['tipo_utente']);
+        $tipo = $row ['tipo_utente'];
     
-    if ($tipo === -1) 
-    {
-        $query = "SELECT id_utente, username, password, docente_referente, docente_tutor FROM utente, docente WHERE id_utente = id_docente AND username = '$usr' AND password = '$psw'";
-        
-        $result = $connessione->query($query);    
-        if ($result && $result->num_rows > 0)
-        {        
-            $row = $result->fetch_assoc();
-            $userId = $row ['id_utente'];
-            $tipo = ($row['docente_tutor'] === "1") ? $tipo = 3 : $tipo = 2;
-            $table = "docente";
-        }    
-    }
-    
-    if ($tipo == -1) {
-        $query = "SELECT id_utente, username, password FROM utente WHERE tipo_utente = 4 AND username = '$usr' AND password = '$psw'";
-        $result = $connessione->query ( $query );
-        if ($result && $result->num_rows > 0) {
-            $row = $result->fetch_assoc ();
-            $tipo = 4;
-            $userId = $row ['id_utente'];
-            $table = "azienda";
+        if ($tipo == studType) {
+            $query_stage = "SELECT studente_has_stage.id_studente_has_stage
+                            FROM studente, studente_has_stage, classe_has_stage, anno_scolastico, studente_attends_classe 
+                            WHERE studente.id_studente = ".$_SESSION ['userId']."
+                            AND studente.id_studente=studente_has_stage.studente_id_studente 
+                            AND studente_has_stage.classe_has_stage_id_classe_has_stage=classe_has_stage.id_classe_has_stage 
+                            AND classe_has_stage.anno_scolastico_id_anno_scolastico=anno_scolastico.id_anno_scolastico 
+                            AND anno_scolastico.corrente=1 
+                            AND studente_attends_classe.anno_scolastico_id_anno_scolastico=anno_scolastico.id_anno_scolastico 
+                            AND studente_attends_classe.classe_id_classe=classe_has_stage.classe_id_classe 
+                            AND studente_attends_classe.studente_id_studente=studente.id_studente;";
+            $result_stage = $connessione->query ( $query_stage );
+            if ($result_stage && $result_stage->num_rows > 0) {
+                $row_stage = $result_stage->fetch_assoc ();
+                $_SESSION ['studenteHasStageId'] = $row_stage ['id_studente_has_stage'];
+            }
         }
-    }
-    
-    if ($tipo == -1) {
-        $query = "SELECT utente.id_utente, utente.username, utente.password FROM utente WHERE tipo_utente = 6 AND username = '$usr' AND password = '$psw'";
-        $result = $connessione->query ( $query );
-        if ($result && $result->num_rows > 0) {
-            $row = $result->fetch_assoc ();
-            $tipo = 6;
-            $userId = $row ['id_utente'];
-            $table = "studente";
-        }
-        $query_stage = "SELECT studente_has_stage.id_studente_has_stage
-                        FROM studente, studente_has_stage, classe_has_stage, anno_scolastico, studente_attends_classe 
-                        WHERE studente.id_studente = $userId 
-                        AND studente.id_studente=studente_has_stage.studente_id_studente 
-                        AND studente_has_stage.classe_has_stage_id_classe_has_stage=classe_has_stage.id_classe_has_stage 
-                        AND classe_has_stage.anno_scolastico_id_anno_scolastico=anno_scolastico.id_anno_scolastico 
-                        AND anno_scolastico.corrente=1 
-                        AND studente_attends_classe.anno_scolastico_id_anno_scolastico=anno_scolastico.id_anno_scolastico 
-                        AND studente_attends_classe.classe_id_classe=classe_has_stage.classe_id_classe 
-                        AND studente_attends_classe.studente_id_studente=studente.id_studente;";
-        $result_stage = $connessione->query ( $query_stage );
-        if ($result_stage && $result_stage->num_rows > 0) {
-            $row_stage = $result_stage->fetch_assoc ();
-            $_SESSION ['studenteHasStageId'] = $row_stage ['id_studente_has_stage'];
-        }
-    }
-    
-    if ($tipo == -1) {
-        $query = "SELECT id_utente, username, password FROM utente WHERE tipo_utente = 5 AND username = '$usr' AND password = '$psw'";
-        $result = $connessione->query ( $query );
-        if ($result && $result->num_rows > 0) {
-            $row = $result->fetch_assoc ();
-            $tipo = 5;
-            $userId = $row ['id_utente'];
-            $table = "tutor";
-        }
-    }
-    if ($tipo != -1) {
-        sessionInformation ( $usr, $userId, $tipo, $table );
     }
     
     $xmlstr = <<<XML
