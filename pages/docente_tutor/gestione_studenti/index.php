2a80e975de46836d96ac98c56564008f520e1fdb
Platinum2d <manicardi215@gmail.com>
2017-03-07 22:42:55 +0100

    Visualizzazione separata della valutazioni + Anno scolastico spostato in basso a destra.

--- pages/docente_tutor/gestione_studenti/index.php
+++ pages/docente_tutor/gestione_studenti/index.php
@@ -27,24 +27,24 @@
     <div class="container">
         <div class="row">
             <div class="col col-sm-12">
-                <div class="panel">
-                    <h1> Le mie classi - A.S. <?php echo $nome_anno_corrente; ?></h1> <br>
-                    
+                <div class="panel" style='min-height: 0'>
+                    <h1>Le mie classi</h1> <br>
+                        
                     <table class="table table-hover">
                         <thead>
                         <th style="text-align: center">
                             Nome classe
                         </th>
-                        
+                            
                         <th style="text-align: center; width: 50%;">
                             Settore di studi
                         </th>
-<!--                        
-                        <th style="width: 15%; text-align: center">
-                            Numero studenti
-                        </th>-->
+                        <!--                        
+                                                <th style="width: 15%; text-align: center">
+                                                    Numero studenti
+                                                </th>-->
                         </thead>
-                        
+                            
                         <tbody style="text-align: center">
                             <?php
                                 //$query = "SELECT DISTINCT c.id_classe, c.nome AS nomeclasse, sect.nome_settore FROM classe AS c, classe_has_docente AS chd, classe_has_stage AS chs , settore AS sect WHERE chd.classe_id_classe = c.id_classe AND chs.classe_id_classe = chd.classe_id_classe AND sect.id_settore = c.settore_id_settore AND chs.anno_scolastico_id_anno_scolastico = $id_anno_corrente AND chd.docente_id_docente = $id_doc";    
@@ -53,7 +53,7 @@
                                         . "WHERE shs.classe_has_stage_id_classe_has_stage = chs.id_classe_has_stage AND chs.classe_id_classe = cl.id_classe "
                                         . "AND sect.id_settore = cl.settore_id_settore AND chs.anno_scolastico_id_anno_scolastico = $id_anno_corrente "
                                         . "AND shs.docente_tutor_id_docente_tutor = $id_doc";
-                            
+                                            
                                 $result = $conn->query($query);
                                 $I=0;
                                 while ($row = $result->fetch_assoc())
@@ -68,6 +68,13 @@
                             ?>
                         </tbody>
                     </table>
+                    <br>
+                    <br>
+                    <br>
+                    <br>
+                    <div align='right'>
+                        <h3 style='display: inline'>A.S. <?php echo $nome_anno_corrente; ?></h3>
+                    </div>
                 </div>
             </div>
         </div>
