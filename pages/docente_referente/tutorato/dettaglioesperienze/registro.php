6e3d3b354149723ea196eaf79d86d59c409e9511
broskh <alessio.scheri@gmail.com>
2017-03-29 02:17:44 +0200

    Modificata visualizzazione del registro per il docente referente.

--- pages/docente_referente/tutorato/dettaglioesperienze/registro.php
+++ pages/docente_referente/tutorato/dettaglioesperienze/registro.php
@@ -1,24 +1,24 @@
 <?php
-    include '../../../functions.php';
-    checkLogin ( docrefType , "../../../../");
-    $conn = dbConnection ("../../../../");    
-    
-    $id_doc = $_SESSION ['userId'];
-    $idanno = $_POST['ida'];
-    $id_studente_has_stage = $_POST['studente_has_stage'];
-    $query = "SELECT stud.nome, stud.cognome FROM studente AS stud, studente_has_stage AS shs WHERE shs.studente_id_studente = stud.id_studente AND shs.id_studente_has_stage = $id_studente_has_stage";
-        
-    $result = $conn->query($query)->fetch_assoc();
-    $cognomestudente = $result['cognome'];
-    $nomestudente = $result['nome'];
-        
-    $nomeanno = $conn->query("SELECT nome_anno AS nome FROM anno_scolastico WHERE id_anno_scolastico = $idanno")->fetch_assoc()['nome'];
-        
-    open_html ( "Registro di $cognomestudente $nomestudente" );
-    
-    import("../../../../");
-    echo "<script src=\"scripts.js\"> </script>";
-        
+include '../../../functions.php';
+checkLogin ( docrefType , "../../../../");
+$conn = dbConnection ("../../../../");
+
+$id_doc = $_SESSION ['userId'];
+$idanno = $_POST['ida'];
+$id_studente_has_stage = $_POST['studente_has_stage'];
+$query = "SELECT stud.nome, stud.cognome FROM studente AS stud, studente_has_stage AS shs WHERE shs.studente_id_studente = stud.id_studente AND shs.id_studente_has_stage = $id_studente_has_stage";
+
+$result = $conn->query($query)->fetch_assoc();
+$cognomestudente = $result['cognome'];
+$nomestudente = $result['nome'];
+
+$nomeanno = $conn->query("SELECT nome_anno AS nome FROM anno_scolastico WHERE id_anno_scolastico = $idanno")->fetch_assoc()['nome'];
+
+open_html ( "Registro di $cognomestudente $nomestudente" );
+
+import("../../../../");
+echo "<script src=\"scripts.js\"> </script>";
+
 ?>
     
 <body>
@@ -37,17 +37,24 @@
                 <div class="panel" style='min-height: 0'>
                     <h1> Registro di <?php echo $cognomestudente . " " . $nomestudente ?></h1> 
                     <br>
-                    <table class='table table-striped'>
-                        <thead>
-                        <th>
-                            Data
-                        </th>
-                        <th>
-                            Attività svolte
-                        </th>
-                        </thead>
-                            
-                        <tbody>
+                    <div class="table-responsive">
+                        <table class='table table-bordered'>
+                            <thead>
+                            <th>
+                                Data
+                            </th>
+                            <th>
+                                Attività svolte
+                            </th>
+                            <th>
+                                Capacità acquisite
+                            </th>
+                            <th>
+                                Commento
+                            </th>
+                            </thead>
+                                
+                            <tbody>
                             <?php
                                 $query = "SELECT * FROM lavoro_giornaliero WHERE studente_has_stage_id_studente_has_stage = $id_studente_has_stage";
                                 $result = $conn->query($query);
@@ -57,14 +64,17 @@
                                     while ($row = $result->fetch_assoc())
                                     {
                                         $data = date("d-m-Y", strtotime($row['data']));
-                                        $descrizione = $row['descrizione'];
+                                        $lavoro_svolto = $row['lavoro_svolto'];
+                                        $insegnamenti = $row['insegnamenti'];
+                                        $commento = $row['commento'];
                                             
-                                        echo "<tr><td>$data</td>  <td>$descrizione</td></tr>";
+                                        echo "<tr><td>$data</td>  <td>$lavoro_svolto</td><td>$insegnamenti</td><td>$commento</td></tr>";
                                     }
                                 }
                             ?>
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                         
                         
                     <br>
