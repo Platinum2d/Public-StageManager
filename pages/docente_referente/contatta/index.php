439bd1e6fbff3a8933c985db4dab92b9eefc717f
broskh <alessio.scheri@gmail.com>
2017-03-30 12:46:11 +0200

    Corretto bug nel contatta del docente referente.

--- pages/docente_referente/contatta/index.php
+++ pages/docente_referente/contatta/index.php
@@ -5,7 +5,7 @@ checkEmail();
 import("../../../");
 open_html ( "Contatta" );
 ?>
-<script src='js/scripts.js'></script>
+<script src='js/scripts.js?0.1'></script>
 <body>
     <?php
         topNavbar ("../../../");
@@ -30,6 +30,7 @@ open_html ( "Contatta" );
                                             <option value="<?php echo doctutType;?>">Docente tutor</option>
                                             <option value="<?php echo studType;?>">Studente</option>
                                             <option value="<?php echo ceoType;?>">Responsabile aziendale</option>
+                                            <option value="<?php echo aztutType;?>">Tutor aziendale</option>
                                         </select>
                                     </div>
                                 	<div class="col col-sm-6">
