ae3b0e7b08352a6a6f64cec96c10110aa6611cba
Platinum2d <manicardi215@gmail.com>
2017-03-31 00:15:07 +0200

    Create pagine utente per gli amministratori

--- pages/visualizza_utente/super_user/alessio/index.php
+++ pages/visualizza_utente/super_user/alessio/index.php
@@ -1,14 +1,14 @@
 <?php
     include '../../../functions.php';
     import("../../../../");
-    open_html ( "Manicardi Daniele" );
+    open_html ( "Scheri Alessio" );
     $connessione = dbConnection ("../../../../");
     $query = "SELECT * FROM utente, super_user WHERE id_utente = id_super_user";
     $result = $connessione->query($query);
     if (is_object($result) && $result->num_rows > 0)
     while ($row = $result->fetch_assoc())
     {
-        if (strtolower($row['cognome']) === "manicardi") break;
+        if (strtolower($row['cognome']) === "scheri") break;
     }
     $nome = $row['nome'];
     $cognome = $row['cognome'];
@@ -25,7 +25,7 @@
         <div class="row">
             <div class="col col-sm-12">
                 <div class="panel">
-                    <h1 style="display: inline">Daniele Manicardi</h1><br><br>
+                    <h1 style="display: inline">Alessio Scheri</h1><br><br>
                     <br>
                     <div class="row">
                         <div class="col col-sm-3" id="profilewrapper" align="center">
@@ -58,16 +58,16 @@
                                     </tr>
                                     <tr>
                                         <th>Mail</th>
-                                        <td id="mail"><?php echo EMAIL_DANIELE?></td>
+                                        <td id="mail"><?php echo EMAIL_ALESSIO?></td>
                                     </tr>
                                 </table>
                             </div>
                             <br>
-                            <div class="row">
+<!--                            <div class="row">
                                 <div class="col col-sm-12" align="right">
                                     <h4 style="display: inline">Disponibile in qualsiasi momento anche su Social e Whatsapp</h4>
                                 </div>
-                            </div>
+                            </div>-->
                         </div>
                     </div>
                 </div>
