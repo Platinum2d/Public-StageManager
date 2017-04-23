c116221792f3d83e945edb5f472d28012f17b617
Platinum2d <manicardi215@gmail.com>
2017-04-23 15:27:27 +0200

    Corretto bug di visualizzazione dei dati del responsabile

--- pages/super_user/visualizza/scuole/scripts/script.js
+++ pages/super_user/visualizza/scuole/scripts/script.js
@@ -25,6 +25,10 @@ function openEdit(numberId, id_scuola)
             var telefono = $(xml).find("scuola").find("telefono").text();
             var email = $(xml).find("scuola").find("email").text();
             var sitoweb = $(xml).find("scuola").find("sito_web").text();
+            var nome_resp = $(xml).find("scuola").find("nome_resp").text();
+            var cognome_resp = $(xml).find("scuola").find("cognome_resp").text();
+            var telefono_resp = $(xml).find("scuola").find("telefono_resp").text();
+            var email_resp = $(xml).find("scuola").find("email_resp").text();
             
             $("<tr id=\"edit"+numberId+"\"> \n\
                     <td></td><td>\n\
@@ -35,7 +39,7 @@ function openEdit(numberId, id_scuola)
                                         <span id=\"userlabel"+numberId+"\">Username</span><input placeholder=\"Username\" type=\"text\" class=\"form-control\" id=\"username"+numberId+"\" value='"+username+"'>\n\
                                         Nome <input class=\"form-control\" id=\"nome"+numberId+"\" value=\""+nome+"\"\">\n\
                                         Città <input class=\"form-control\" id=\"citta"+numberId+"\" value=\""+citta+"\"\">\n\
-                                        CAP <input type=\"number\" min='00000' class=\"form-control\" id=\"CAP"+numberId+"\" value=\""+CAP+"\"\">\n\
+                                        CAP <input type=\"number\" min='0' class=\"form-control\" id=\"CAP"+numberId+"\" value=\""+CAP+"\"\">\n\
                                         Indirizzo <input class=\"form-control\" id=\"indirizzo"+numberId+"\" value=\""+indirizzo+"\"\">\n\
                                     </div>\n\
                                     <div class=\"col col-sm-6\">\n\
@@ -43,10 +47,10 @@ function openEdit(numberId, id_scuola)
                                         Telefono <input class=\"form-control\" id=\"telefono"+numberId+"\" value=\""+telefono+"\"\">\n\
                                         E-Mail <input class=\"form-control\" id=\"email"+numberId+"\" value=\""+email+"\"\">\n\
                                         Sito Web <input class=\"form-control\" id=\"sitoweb"+numberId+"\" value=\""+sitoweb+"\"\">\n\
-                                        Nome Responsabile <input class=\"form-control\" id=\"nomeResponsabile"+numberId+"\" value=\""+sitoweb+"\"\">\n\
-                                        Cognome Responsabile <input class=\"form-control\" id=\"cognomeResponsabile"+numberId+"\" value=\""+sitoweb+"\"\">\n\
-                                        Telefono Responsabile <input class=\"form-control\" id=\"telefonoResponsabile"+numberId+"\" value=\""+sitoweb+"\"\">\n\
-                                        Email Responsabile <input class=\"form-control\" id=\"mailResponsabile"+numberId+"\" value=\""+sitoweb+"\"\"><br>\n\
+                                        Nome Responsabile <input class=\"form-control\" id=\"nomeResponsabile"+numberId+"\" value=\""+nome_resp+"\"\">\n\
+                                        Cognome Responsabile <input class=\"form-control\" id=\"cognomeResponsabile"+numberId+"\" value=\""+cognome_resp+"\"\">\n\
+                                        Telefono Responsabile <input class=\"form-control\" id=\"telefonoResponsabile"+numberId+"\" value=\""+telefono_resp+"\"\">\n\
+                                        Email Responsabile <input class=\"form-control\" id=\"mailResponsabile"+numberId+"\" value=\""+email_resp+"\"\"><br>\n\
                                         <button id=\"save"+numberId+"\" class=\"btn btn-success btn-sm rightAlignment margin buttonfix\" onclick=\"sendData("+numberId+", "+id_scuola+")\"><span class=\"glyphicon glyphicon-ok\"></span></button>\n\
                                         <button id=\"cancel"+numberId+"\" class=\"btn btn-danger btn-sm rightAlignment margin buttonfix\" onclick=\"closeEdit("+numberId+")\"><span class=\"glyphicon glyphicon-remove\"></span></button>\n\
                                     </div>\n\
