da65b411bcfb003352edbc09b8872019e8e23d14
Platinum2d <manicardi215@gmail.com>
2017-04-28 20:10:12 +0200

    Aggiornato al nuovo database la gestione dei docenti referenti da parte della scuola

--- pages/scuola/gestione/classi_docenti_referenti/index.php
+++ pages/scuola/gestione/classi_docenti_referenti/index.php
@@ -2,12 +2,13 @@
     include '../../../functions.php';
     checkLogin ( scuolaType , "../../../../");
     open_html ( "Classi e docenti referenti" );
-    echo "<script src='scripts/script.js'></script>";
+    echo "<script src='scripts/script.js?8'></script>";
     import("../../../../");
     $conn = dbConnection("../../../../");
     $resultanno = $conn->query("SELECT * FROM anno_scolastico WHERE corrente = 1")->fetch_assoc();
     $nomeanno = $resultanno['nome_anno'];
     $idanno = $resultanno['id_anno_scolastico'];
+    $idscuola = $_SESSION['userId'];
 ?>
 
 <body>
@@ -60,62 +61,43 @@
                     <table class="table table-bordered" id='mainTable'>
                         <thead>
                         <th>
-                            Classe assegnata
+                            Classe
                         </th>
                         <th>
                             Docente referente assegnato
-                        </th> 
-                        <th>
-                            Gestiti su Totali
-                        </th> 
+                        </th>
                         <th style="text-align:center">
                             Azioni
                         </th>
                         </thead>
                         <tbody>
                             <?php
-                                $classesquery = "SELECT id_classe, nome FROM classe WHERE scuola_id_scuola = ".$_SESSION['userId'];
-                                    
-                                $resultclasses = $conn->query($classesquery);
-                                while ($row = $resultclasses->fetch_assoc())
+                                $query = "  SELECT id_docente_has_classe_has_stage AS id_drhchs, doc.nome AS nomedocente, doc.cognome AS cognomedocente, classe.nome AS nomeclasse 
+                                            FROM docente AS doc, docente_referente_has_classe_has_stage AS drhchs, classe_has_stage AS chs, classe 
+                                            WHERE drhchs.docente_id_docente = doc.id_docente 
+                                            AND chs.id_classe_has_stage = drhchs.classe_has_stage_id_classe_has_stage 
+                                            AND classe.id_classe = chs.classe_id_classe 
+                                            AND doc.scuola_id_scuola = $idscuola 
+                                            AND chs.anno_scolastico_id_anno_scolastico = $idanno
+                                            AND classe.scuola_id_scuola = $idscuola";
+                                                                
+                                $result = $conn->query($query);
+                                if (is_object($result) && $result->num_rows > 0)
                                 {
-                                    $current_classe_id = $row['id_classe'];
-                                    $current_nome_classe = $row['nome'];
-                                        
-                                                                            //quanti studenti ha la classe corrente in quest'anno
-                                    $num_stud_current_class_query = "SELECT COUNT(sac.id_studente_attends_classe) AS count "
-                                                                                 . "FROM studente_attends_classe AS sac "
-                                                                                 . "WHERE sac.classe_id_classe = $current_classe_id "
-                                                                                 . "AND sac.anno_scolastico_id_anno_scolastico = $idanno";                                        
-                                    $num_stud_current_class = intval($conn->query($num_stud_current_class_query)->fetch_assoc()['count']);
-                                        
-                                    $docsquery = "SELECT id_docente, nome, cognome FROM utente, docente WHERE id_utente = id_docente AND tipo_utente = ".docrefType;
-                                    $resultdocs = $conn->query($docsquery);
-                                    while ($rowdocs = $resultdocs->fetch_assoc())
+                                    $i = 0;
+                                    while ($row = $result->fetch_assoc())
                                     {
-                                        $current_docente_id = $rowdocs['id_docente'];
-                                        $current_nome_docente = $rowdocs['nome'];
-                                        $current_cognome_docente = $rowdocs['cognome'];
-                                            
-                                            
-                                        //quanti studenti ha gestito il docente corrente in questa classe? Se è pari a $num_stud_current_class, allora è un docente referente di questa classe
-                                        $num_stud_gestiti_current_doc_query = "SELECT COUNT(drhshs.id_docente_has_studente_has_stage) AS count 
-                                                                                             FROM studente_has_stage AS shs, classe_has_stage AS chs, docente_referente_has_studente_has_stage AS drhshs 
-                                                                                             WHERE drhshs.studente_has_stage_id_studente_has_stage = shs.id_studente_has_stage 
-                                                                                             AND shs.classe_has_stage_id_classe_has_stage = chs.id_classe_has_stage 
-                                                                                             AND chs.classe_id_classe = $current_classe_id 
-                                                                                             AND chs.anno_scolastico_id_anno_scolastico = $idanno 
-                                                                                             AND drhshs.docente_id_docente = $current_docente_id;";
-                                        $num_stud_gestiti_current_doc = intval($conn->query($num_stud_gestiti_current_doc_query)->fetch_assoc()['count']);
-                                            
-                                        //echo $current_cognome_docente . " " . $current_nome_docente . " gestisce ". $num_stud_gestiti_current_doc . " in ".$current_nome_classe. " su ".$num_stud_current_class. " totali<br>";
-                                            
-                                        if ($num_stud_current_class > 0 && $num_stud_gestiti_current_doc > 0)
-                                        {
-                                            echo "<tr><td align='center'>$current_nome_classe</td><td>$current_nome_docente $current_cognome_docente</td>"
-                                                    . "<td align='center'><u style='cursor : pointer' onclick='showAssignedStudents($current_docente_id, $current_classe_id, $idanno)'>$num_stud_gestiti_current_doc su $num_stud_current_class</u></td>"
-                                                    . "<td align='center'><button onclick='deleteAssignment($current_docente_id, $current_classe_id, $idanno)' class='btn btn-danger'><span class='glyphicon glyphicon-trash'></span> Disassegna</button></td></tr>";
-                                        }
+                                        $nomedocente = $row['nomedocente'];
+                                        $cognomedocente = $row['cognomedocente'];
+                                        $nomeclasse = $row['nomeclasse'];
+                                        $id_drhchs = $row['id_drhchs'];
+                                        
+                                        echo "<tr id='riga$i'>";
+                                            echo "<td>$nomeclasse</td> <td>$nomedocente $cognomedocente</td> "
+                                               . "<td align='center'><button onclick='askForDeleteAssignment($i, $id_drhchs, $idanno)' class='btn btn-danger'><span class='glyphicon glyphicon-trash'></span> Disassegna</button> </td>";
+                                        echo "</tr>";
+                                        
+                                        $i++;
                                     }
                                 }
                             ?>
--- pages/scuola/gestione/classi_docenti_referenti/scripts/script.js
+++ pages/scuola/gestione/classi_docenti_referenti/scripts/script.js
@@ -21,17 +21,35 @@ function loadValidClasses(id_anno)
     });
 }
 
-function deleteAssignment(id_doc, id_classe, id_anno)
+function askForDeleteAssignment(progressiv, id_drhchs, id_anno)
+{
+    $("#SuperAlert").modal("show");
+    var modal = $("#SuperAlert").find(".modal-body");
+    $("#SuperAlert").find(".modal-title").html("ATTENZIONE");
+    modal.html("<div align='center'><h2> Procedere con l'eliminazione della seguente assegnazione? </h2></div>");
+    $("#SuperAlert").find(".modal-footer").html("<div class='row'> \n\
+                                                    <div class='col col-sm-6' align='left'></div>\n\
+                                                    <div class='col col-sm-6'> \n\
+                                                        <button class='btn btn-success' onclick=\"deleteAssignment("+progressiv+", "+id_drhchs+", "+id_anno+")\" data-dismiss='modal'>Si</button>\n\
+                                                        <button class='btn btn-danger' data-dismiss='modal'>No</button>\n\
+                                                    </div> \n\
+                                                 </div>");
+}
+
+function deleteAssignment(progressiv, id_drhchs, id_anno)
 {
     $.ajax({
         type : 'POST',
         url : 'ajaxOpsPerClassiDocentiReferenti/ajaxDisassegna.php',
         cache : false,
-        data : {'docente' : id_doc, 'classe' : id_classe, 'anno' : id_anno},
+        data : {'drhchs' : id_drhchs},
         success : function (msg)
         {
             if (msg === "ok")
-                location.reload();
+            {
+                $("#riga"+progressiv).fadeOut("slow");
+                loadValidClasses(id_anno);
+            }
         }
     });
 }
@@ -50,63 +68,6 @@ function assignDocToClass(id_anno)
         {
             if (msg === "ok")
                 location.reload();
-        }
-    });
-}
-
-function showAssignedStudents(id_doc, id_classe, id_anno)
-{
-    $.ajax({
-        type : 'POST',
-        url : 'ajaxOpsPerClassiDocentiReferenti/ajaxStudentiAssegnati.php',
-        cache : false,
-        data : {'docente' : id_doc, 'classe' : id_classe, 'anno' : id_anno},
-        success : function (xml)
-        {
-            $("#SuperAlert").modal("show");
-            $("#SuperAlert").find(".modal-title").html("Studenti assegnati al professore");
-            var modal = $("#SuperAlert").find(".modal-body");
-            modal.html("<table class='table table-hover' id='studtable'>\n\
-                            <thead>\n\
-                                <th style='text-align:center'>Nome e cognome</th>\n\
-                                <th style='text-align:center'>Azioni</th>\n\
-                            </thead>\n\
-                            <tbody>\n\
-                            </tbody>\n\
-                        </table>");
-            
-            $(xml).find("studenti").find("studente").each(function (){
-                var id = $(this).find("id").text();
-                var id_drhshs = $(this).find("id_drhshs").text();
-                var nome = $(this).find("nome").text();
-                var cognome = $(this).find("cognome").text();
-                
-                $("#studtable").find("tbody").append("<tr data-id='"+id+"'>\n\
-                                                        <td align='center'><u style='cursor:pointer' onclick='userProfile("+id+", \"../../../\")'>"+nome+" "+cognome+"</u></td>\n\
-                                                        <td align='center'><button onclick='disassegnaStudente("+id_drhshs+", this)' class='btn btn-danger'><span class='glyphicon glyphicon-trash'></span> Disassegna</button></td>\n\
-                                                      </tr>");
-            });
-        }
-    });
-}
-
-function disassegnaStudente(id_drhshs, btn)
-{
-    $.ajax({
-        type : 'POST',
-        url : 'ajaxOpsPerClassiDocentiReferenti/ajaxDisassegnaStudente.php',
-        cache : false,
-        data : {'drhshs' : id_drhshs},
-        success : function (msg)
-        {
-            if (msg === "ok")
-            {
-                $(btn).parents("tr").fadeOut();
-            }
-            else
-            {
-                printError("Errore", "<div align='center'>Errore durante l'eliminazione di questa assegnazione.<br>Si prega di segnalare il problema</div>");
-            }
         }
     });
 }
