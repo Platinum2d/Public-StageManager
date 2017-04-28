8dfdcf014c89523869fd3b8c40f291bb046b5d59
Platinum2d <manicardi215@gmail.com>
2017-04-28 20:11:29 +0200

    Aggiornata al nuovo db la parte della gestione dei periodi di stage nel super user

--- pages/super_user/visualizza/stage/scripts/script.js
+++ pages/super_user/visualizza/stage/scripts/script.js
@@ -1,21 +1,55 @@
 $(document).ready(function (){
-    $("#checkall").change(function (){
-        if ($(this).prop("checked"))
-            $(".singlecheck").prop("checked", true);
-        else
-            $(".singlecheck").prop("checked", false);
+    $.datepicker.setDefaults( $.datepicker.regional[ "it" ] );
+    $("input[name='data_inizio']").each(function (){
+        $(this).datepicker({dateFormat : 'dd-mm-yy'});
+        
+        $(this).change(checkInizio);
+    });
+    
+    $("input[name='data_fine']").each(function (){
+        $(this).datepicker({dateFormat : 'dd-mm-yy', minDate : $('#inizio'+$(this).attr("data-id")).val()});
+        $('#fine'+$(this).attr("data-id")).datepicker("setDate", new Date ($('#fine'+$(this).attr("data-id")).val()));
+        $(this).change(checkFine);
     });
 });
+
+function checkInizio()
+{
+    $(this).css('color', 'red');
+    $('#fine'+$(this).attr("data-id")).datepicker('destroy');
+    $('#fine'+$(this).attr("data-id")).datepicker({dateFormat : 'dd-mm-yy', minDate : $('#inizio'+$(this).attr("data-id")).val()});
+    
+    var start = new Date($('#inizio'+$(this).attr("data-id")).datepicker("getDate"));
+    var end = new Date($('#fine'+$(this).attr("data-id")).datepicker("getDate"));
+    if (end <= start)
+    {
+        $('#fine'+$(this).attr("data-id")).val("");
+    }
+}
+
+function checkFine()
+{
+    $(this).css('color', 'red');
+    
+    var start = new Date($('#inizio'+$(this).attr("data-id")).datepicker("getDate"));
+    var end = new Date($('#fine'+$(this).attr("data-id")).datepicker("getDate"));
+    
+    if (end <= start)
+    {
+        $('#inizio'+$(this).attr("data-id")).val("");
+    }
+}
 
 function sendData(numberId, id_stage)
 {
     tosend = {
         'id' : id_stage,
-        'inizio' : $("#stage"+numberId).find("input[name='datawrapper']").val(),
-        'durata' : $("#stage"+numberId).find("td[name='durata']").html()
+        'inizio' : $("#inizio"+numberId).val(),
+        'fine' : $("#fine"+numberId).val()
     };
     
-    if (tosend.inizio.trim() === "" || typeof(tosend.inizio.trim()) === "undefined" || tosend.durata.trim() === "" || typeof(tosend.durata.trim()) === "undefined")
+    
+    if (tosend.inizio.trim() === "" || typeof(tosend.inizio.trim()) === "undefined" || tosend.fine.trim() === "" || typeof(tosend.fine.trim()) === "undefined")
         return;
     
     $.ajax({
@@ -30,7 +64,8 @@ function sendData(numberId, id_stage)
     });
 }
 
-function resetColors(numberId){
-    $("#stage"+numberId).find("input[name='datawrapper']").css("color", "#828282");
-    $("#stage"+numberId).find("td[name='durata']").css("color", "#828282");
+function resetColors(numberId)
+{
+    $("#inizio"+numberId).css("color", "#828282");
+    $("#fine"+numberId).css("color", "#828282");
 }
