2416315251ac0da2f61aeae3508810920c090ab3
Platinum2d <manicardi215@gmail.com>
2017-05-06 19:11:07 +0200

    Corretto bug di reload e aggiunta eliminazione delle opzioni automatica al cambio di tipo risposta.

--- pages/scuola/gestione/moduli_valutazione/moduli_valutazione_studenti/visualizza_modulo/ajaxOpsPerVisualizzaModuloStudente/ajaxEditColumn.php
+++ pages/scuola/gestione/moduli_valutazione/moduli_valutazione_studenti/visualizza_modulo/ajaxOpsPerVisualizzaModuloStudente/ajaxEditColumn.php
@@ -6,13 +6,21 @@
     $idcolonna = $_POST['id'];
     $descrizione = $conn->escape_string($_POST['nome']);
     $posizione = $_POST['posizione'];
-    $tiporisposta = $_POST['risposta'];
+    $tiporisposta = intval($_POST['risposta']);
+    
+    $errore = false;
+    
+    if ($tiporisposta === 0)
+    {
+        $query_opzioni = "DELETE FROM possibile_risposta_colonna_studente WHERE colonna_modulo_studente_id_colonna_modulo_studente = $idcolonna";
+        if (!$conn->query($query_opzioni)) $errore = true; 
+    }
     
     $query = "UPDATE colonna_modulo_studente "
             . "SET descrizione = '$descrizione', numero_voce = $posizione, risposta_multipla = $tiporisposta "
             . "WHERE id_colonna_modulo_studente = $idcolonna";
     
-    if ($conn->query($query))
+    if (!$errore && $conn->query($query))
         echo "ok";
     else
         echo $conn->error;
