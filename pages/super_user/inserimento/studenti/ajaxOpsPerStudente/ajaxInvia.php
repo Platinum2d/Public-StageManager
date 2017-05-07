1925fceb5137fc76427bcd3441dffef8754f7b19
Platinum2d <manicardi215@gmail.com>
2017-05-07 16:47:16 +0200

    Eliminate query a docente_referente_has_studente_has_stage

--- pages/super_user/inserimento/studenti/ajaxOpsPerStudente/ajaxInvia.php
+++ pages/super_user/inserimento/studenti/ajaxOpsPerStudente/ajaxInvia.php
@@ -25,7 +25,33 @@
 
     $connection->query("SET FOREIGN_KEY_CHECKS = 0");
     if ($connection->query($userquery) && $connection->query($Query) && $connection->query($classquery))
-        echo "ok";
+    {
+        $query = "SELECT id_classe_has_stage FROM classe_has_stage WHERE classe_id_classe = $classe AND anno_scolastico_id_anno_scolastico = $annoscolastico";
+        
+        $result = $connection->query($query);
+        $errore = false;
+        if ($result)
+        {
+            while ($row = $result->fetch_assoc())
+            {
+                $id_classe_has_stage = $row['id_classe_has_stage'];
+
+                $query = "INSERT INTO studente_has_stage ("
+                        . "visita_azienda, autorizzazione_registro, studente_id_studente, "
+                        . "classe_has_stage_id_classe_has_stage, azienda_id_azienda, "
+                        . "docente_tutor_id_docente_tutor, tutor_id_tutor) "
+                        . "VALUES (0, 1, (SELECT MAX(id_studente) FROM studente), "
+                        . "$id_classe_has_stage, NULL, "
+                        . "NULL, NULL)";
+                if (!$connection->query($query))
+                    $errore = true;
+            }
+        }
+        if (!$errore)
+            echo "ok";
+        else
+            echo $connection->error;
+    }
     else
         echo $connection->error;
 ?>
