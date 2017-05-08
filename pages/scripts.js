2276710da10ae588341933be704fa1122891232d
broskh <alessio.scheri@gmail.com>
2017-05-08 12:13:34 +0200

    Risolti piccoli bug.

--- pages/scripts.js
+++ pages/scripts.js
@@ -151,7 +151,7 @@ function checkEmptyFields (fields) {
 	if (Array.isArray (fields)) {
 		fields.forEach (function (item) {
 			$(item).each (function () {
-				if ($(this).val().trim().isEmpty()) {
+				if ($(this).val() && $(this).val().trim().isEmpty()) {
 					empty = true;
 					return;
 				}
